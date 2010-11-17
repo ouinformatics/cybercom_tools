cdee3f68399ac48fcc5071f9efef1c920fcf77fd
Jonah Duckles <jduckles@dhcp-104-41.rccc.ou.edu>
2010-11-17 09:22:58 -0600

    New model Vegetation Photosynthesis Model (VPM)

---
+++ vpm/modis_indexes.py
@@ -0,0 +1,46 @@
+
+import urllib
+import csv
+
+
+def get_eomf_ts( modis_product, years, lat, lon ):
+    """ Get MODIS data product from EOMF webservice"""
+    url_string = 'http://eomf.ou.edu/visualization/csv_%s_%s_%s_%s.csv' % (modis_product, years, lat, lon)
+    ts = csv.reader(urllib.urlopen(url_string))
+    header = ts.next()
+    output = []
+    for line in ts:
+        output.append(dict(zip(header, line)))
+    return output 
+
+def compute_evi( nir_val, red_val, blue_val, G=2.5, L=1, C1=6, C2=7.5 ):
+    """ Compute the EVI based on NIR, RED and BLUE Bands """
+    # Coefficients are set to standards adopted for MODIS EVI:
+    # G = 2.5   gain factor
+    # L = 1     Canopy bg adjustment
+    # C1 = 6    Coefficient of aerosol resistance 1
+    # C2 = 7.5  Coefficient of aerosol resistance 2
+    return G * ( nir_val - red_val) / ( nir_val + (C1 * red_val) - (C2 * blue_val) + L)
+
+def compute_ndvi( nir_val, red_val ):
+    """ Compute the NDVI based on NIR and RED bands """ 
+    return (nir_val - red_val) / (nir_val + red_val)
+
+def compute_lswi( nir_val, swir_val ):
+    """ Compute the Land Surface Water Index """
+    return ( nir_val - swir_val) / (nir_val + swir_val)
+
+def compute_tscalar( T, T_min, T_max, T_opt ):
+    """ Compute T_scalar """
+    return (( T - T_min ) * ( T - T_max )) / ( ( ( T - T_min ) * ( T - T_max ) ) - ( ( T - T_opt ) * ( T - T_opt ) ) )
+
+def compute_wscalar( LSWI, LSWI_max ):
+    """ Estimate water scalar from LSWI.  LSWI_max is the max across the growing season """
+    return (1 + LWSI) / (1 + LSWI_max)
+
+    
+def scale( val, factor):
+    """ Apply scaling factor """ 
+    return (val / factor)
+
+
