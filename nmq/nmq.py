7c7a429ea05dca86cdd349646dac04ada7a43bcc
Jonah Duckles <jduckles@fire.rccc.ou.edu>
2010-12-21 13:49:57 -0600

    Fixed misalignment

--- nmq/nmq.py
+++ nmq/nmq.py
@@ -1,46 +1,38 @@
 import numpy
-import sys
-import subprocess 
+import subprocess
+import sys 
 from osgeo import gdal
 from osgeo.gdalconst import *
 import osgeo.osr as osr
 
-EXEC_PATH='/home/bcremeans/nmq/NMQ_CartBinaryReader/read_nmq'
-#FILE_PATH='/scratch/data/nws/nexrad/tile6/unqc_cref/'
-#OUT_FILE='/home/bcremeans/nmq/NMQ_CartBinaryReader/out.gtiff'
-
-def usage():
-	print "usage: python read_nmq inputfile outputfile"
-
-try:
-	FILE_PATH=sys.argv[1]
-	OUT_FILE=sys.argv[2]
-except:
-	usage()
-	sys.exit(2)
-
+filein = sys.argv[1]
+fileout = sys.argv[2]
+EXEC_PATH='/home/jduckles/cybercom/nmq/read_nmq'
+FILE_PATH='/scratch/data/nws/nexrad/tile6/unqc_cref/'
 format='GTiff'
 
-cline=[EXEC_PATH,FILE_PATH+'UNQC_CREF.20090728.112000','0']
+
+# Run read_nmq to read custom binary file format
+cline=[EXEC_PATH,filein,'0']
 cmd=subprocess.Popen(cline,stdout=subprocess.PIPE)
 oput=cmd.communicate()[0].strip().split('\n')
+
 headers=oput[:24]
 data=oput[25:]
-data.reverse()
+#data.reverse()
 for i in range(0,len(data)):
 	data[i]=data[i].split('\t')
 
 npdata=numpy.array(data)
 npdata=numpy.cast['float'](npdata)
-npdata=numpy.rot90(npdata,3)
+npdata=numpy.rot90(npdata)
 
 driver = gdal.GetDriverByName( format )
-dst_ds = driver.Create( OUT_FILE, 2001, 2001, 1, gdal.GDT_Float32)
-dst_ds.SetGeoTransform( [ 40.005, 0.01, 0, -110.005, 0, 0.01 ] )
+dst_ds = driver.Create( fileout, 2001, 2001, 1, gdal.GDT_Float32)
+dst_ds.SetGeoTransform( [ -110.005, 0.01, 0, 40.005, 0, -0.01 ] )
 srs = osr.SpatialReference()
 srs.SetWellKnownGeogCS("WGS84")
 dst_ds.SetProjection( srs.ExportToWkt() )
 dst_ds.GetRasterBand(1).WriteArray(npdata)
 dst_ds=None
-
 
