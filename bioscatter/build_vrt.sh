9b8af2462c20fd2eeaab176cb401a4c9cfa304ef
Jonah Duckles <jduckles@fire.rccc.ou.edu>
2011-02-23 11:45:22 -0600

    Replaced ls with find to avoid "too many arugments" problem.

--- bioscatter/build_vrt.sh
+++ bioscatter/build_vrt.sh
@@ -14,10 +14,10 @@ findFiles() {
     VRT=$2
     if [ -z $VRT ]; then
         # Count the number of files in the 8 tiles directories and only return if all 8 tiles are in place.
-        ls -1 ${DATAPATH}/tile{1..8}/${PRODUCT}/*.gtiff | cut -d'.' -f2,3 |sort | grep '^[0-9]\{8\}' | uniq -c | grep '^      8 ' | sed 's/^      8 //g'
+        find ${DATAPATH}/tile*/${PRODUCT}/ -name *.gtiff| cut -d'.' -f2,3 |sort | grep '^[0-9]\{8\}' | uniq -c | grep '^      8 ' | sed 's/^      8 //g'
     else
         # Check if VRT has been built
-        ls -1 ${DATAPATH}/mosaic/${PRODUCT}/*.vrt| sed 's/_aea//g' | cut -d'.' -f2,3 | sort | grep '^[0-9]\{8\}' | uniq
+        find ${DATAPATH}/mosaic/${PRODUCT}/ -name *.vrt| sed 's/_aea//g' | cut -d'.' -f2,3 | sort | grep '^[0-9]\{8\}' | uniq
     fi
 }
 
@@ -47,10 +47,11 @@ buildVRT() {
     else
         TIMESTEPS=$(findFiles ${PRODUCT}) # Build VRTs for all files
     fi
+
     for TIMESTEP in $TIMESTEPS; do 
         gdalbuildvrt -overwrite -srcnodata "-999" $DATAPATH/mosaic/$PRODUCT/$PRODUCT_UP.$TIMESTEP.vrt $(listFiles $PRODUCT $TIMESTEP)
         if [ -f $DATAPATH/mosaic/$PRODUCT/$PRODUCT_UP.$TIMESTEP_aea.vrt ]; then rm $DATAPATH/mosaic/$PRODUCT/$PRODUCT_UP.$TIMESTEP_aea.vrt; fi
-        gdalwarp -t_srs /scratch/data/aea2.proj4 -srcnodata "-99" -of VRT $DATAPATH/mosaic/$PRODUCT/$PRODUCT_UP.$TIMESTEP.vrt $DATAPATH/mosaic/$PRODUCT/$PRODUCT_UP.${TIMESTEP}_aea.vrt
+        gdalwarp -t_srs /scratch/data/aea2.proj4 -srcnodata "-99" -of VRT $DATAPATH/mosaic/$PRODUCT/$PRODUCT_UP.$TIMESTEP.vrt $DATAPATH/mosaic/$PRODUCT/$PRODUCT_UP.${TIMESTEP}_aea.vrt 
     done
 }
 
