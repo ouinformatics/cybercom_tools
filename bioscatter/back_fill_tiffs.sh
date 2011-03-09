2ed9a8f2f938e6fdc2e25c18a4234f54f0b59a8b
Jonah Duckles <jduckles@fire.rccc.ou.edu>
2011-03-09 10:12:57 -0600

    Reduce number of CPUs to lower IO load

--- bioscatter/back_fill_tiffs.sh
+++ bioscatter/back_fill_tiffs.sh
@@ -3,5 +3,5 @@ PRODUCT=$1
 
 for tile in tile{1..8}; do 
     dir=/scratch/data/nws/ldm/tiles/${tile}/${PRODUCT};
-    find ${dir} -name *.gz | parallel "if [ -f {.}.gtiff ]; then echo -n '.';  else echo -n '#'; /scratch/tools/nmq.py {} {.}.gtiff; fi"
+    find ${dir} -name *.gz | parallel -P 7 "if [ -f {.}.gtiff ]; then echo -n '.';  else echo -n '#'; /scratch/tools/nmq.py {} {.}.gtiff; fi"
 done
