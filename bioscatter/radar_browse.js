ccb648ca927903837dc5c6561773023e11160e42
Jonah Duckles <jduckles@fire.rccc.ou.edu>
2011-03-15 16:09:44 -0500

    Streamlining code

--- bioscatter/radar_browse.js
+++ bioscatter/radar_browse.js
@@ -1,22 +1,52 @@
 $(document).ready(function() {
 
+var minDate = new Date('November 1, 2008 00:00:00 GMT');
+var maxDate = new Date('March 31, 2011 00:00:00 GMT');
 
-var minDate = new Date(2008, 11-1, 1);
-var minDate_utc = new Date( minDate.getTime() + minDate.getUTCOffset() * 36000000 );
-var maxDate = new Date(2011, 3-1, 14);
-    
-//$('#date').datepicker({ minDate: new Date(2009, 11 - 1, 1), maxDate: '+1D'} );
+var loc = '35.75,-88.25,37.75,-86.25'
+
+serialize = function(obj) {
+  var str = [];
+  for(var p in obj)
+     str.push(p + "=" + encodeURIComponent(obj[p]));
+  return str.join("&");
+};
+
+$('#scene').val(maxDate.toISOString())
+
+
+function makeUrl(params) {
+    defparams = { 
+        MAP:        '/scratch/www/map/radar_wms.map',   
+        SERVICE:    'WMS',
+        version:    '1.3.0',
+        REQUEST:    'GetMap',
+        layers:     'nexrad-unqc_cref',
+        CRS:        'epsg:4326',
+        time:       params.datetime,
+        WIDTH:      500,
+        HEIGHT:     500,
+        BBOX:       params.loc,
+        FORMAT:     'image/png'
+    };
+    var url = '/cgi-bin/mapserv?' + serialize(defparams)
+    return url
+};
+
+function updateUI(datetime) {
+     $('#scene').val(datetime);
+     $('#minute').val( new Date(datetime).setTimezone('GMT').toString('HH:mm') ); 
+     $('#day').val( new Date(datetime).setTimezone('GMT').toString('M/d/yyyy') );
+}
 
 $('#daySlider').slider( { value: maxDate,  
     max: maxDate.getTime(), 
     min: minDate.getTime(), 
     step: 86400000,
     slide: function(event, ui) {
-        var day = new Date( ui.value ).toString('M/d/yyyy'); 
-        $('#day').val( day );
-        datetime =  new Date(ui.value + $('#minSlider').slider('value')).toISOString() 
-        $('#scene').val( datetime);
-        $(img).attr('src','/cgi-bin/mapserv?MAP=/scratch/www/map/radar_wms.map&SERVICE=WMS&version=1.3.0&REQUEST=GetMap&layers=nexrad-unqc_cref&CRS=epsg:4326&time='+datetime+'&WIDTH=500&HEIGHT=500&BBOX=35.75,-88.25,37.75,-86.25&FORMAT=image/png');
+        datetime =  new Date(ui.value + $('#minSlider').slider('value')).toISOString();
+        $(img).attr('src',makeUrl({datetime: datetime, loc: loc}) );
+        updateUI(datetime);
     } 
 }); 
 
@@ -25,14 +55,9 @@ $('#minSlider').slider( { value: 0,
     max: 86400000, 
     step: 300000, 
     slide: function(event, ui) { 
-        var minute = new Date( ui.value ).toString('HH:mm');
-        $('#minute').val( minute ); 
-        datetime = new Date(ui.value + $('#daySlider').slider('value')).toISOString() 
-        $('#scene').val( datetime );
-        //$(img).attr('src', '/cgi-bin/mapserv?MAP=/scratch/www/map/radar_wms.map&SERVICE=WMS&version=1.3.0&REQUEST=GetMap&layers=nexrad-unqc_cref&CRS=epsg:4326&time='+datetime+'&WIDTH=2000&HEIGHT=1000&BBOX=17,-131,57,-58&FORMAT=image/png')
-        $(img).attr('src', '/cgi-bin/mapserv?MAP=/scratch/www/map/radar_wms.map&SERVICE=WMS&version=1.3.0&REQUEST=GetMap&layers=nexrad-unqc_cref&CRS=epsg:4326&time='+datetime+'&WIDTH=500&HEIGHT=500&BBOX=35.75,-88.25,37.75,-86.25&FORMAT=image/png');
-        //loadImage(datetime);
-
+        datetime = new Date(ui.value + $('#daySlider').slider('value')).toISOString();
+        $(img).attr('src', makeUrl({datetime: datetime, loc: loc}) );
+        updateUI(datetime);
     }
 }); 
 
@@ -62,7 +87,7 @@ $('#minSlider').slider( { value: 0,
     })
     
     // *finally*, set the src attribute of the new image to our image
-    .attr('src', '/cgi-bin/mapserv?MAP=/scratch/www/map/radar_wms.map&SERVICE=WMS&version=1.3.0&REQUEST=GetMap&layers=nexrad-unqc_cref&CRS=epsg:4328&time=2011-03-11T13:00:00Z&WIDTH=500&HEIGHT=500&BBOX=17,-131,57,-58&FORMAT=image/png');
+    .attr('src', getUrl(datetime, loc));
 
 
 });
