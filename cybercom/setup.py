e04bba9d850eb04f19d8ac35cf9d9aac437af094
Jonah Duckles <jduckles@ou.edu>
2011-05-20 13:46:10 -0500

    Added forgotten dependencies

--- cybercom/setup.py
+++ cybercom/setup.py
@@ -1,8 +1,8 @@
-import ez_setup
-ez_setup.use_setuptools()
+#import ez_setup
+#ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='cybercom',
       version='0.0',
       packages= find_packages(),
-      install_requires = ['pymongo', 'psycopg2', 'iso8601']      
+      install_requires = ['pymongo', 'psycopg2', 'iso8601', 'simplejson', 'sqlalchemy']      
 )
