23850dd9f39d0d153b76b8ff4c320d213fb40500
Jonah Duckles <jduckles@ou.edu>
2011-11-22 16:50:16 -0600

    Was testing out json serializiation setting

--- wsgi_apps/queue/celeryconfig.py
+++ wsgi_apps/queue/celeryconfig.py
@@ -4,6 +4,8 @@ BROKER_USER = "jduckles"
 BROKER_PASSWORD = "cybercommons"
 BROKER_VHOST = "cybercom_test"
 
+#CELERY_RESULT_SERIALIZER = "json"
+
 CELERY_RESULT_BACKEND = "mongodb"
 CELERY_MONGODB_BACKEND_SETTINGS = {
     "host": "fire.rccc.ou.edu",
