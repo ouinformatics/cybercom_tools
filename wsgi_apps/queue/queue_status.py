9f104e063d4bf581c843ac1e46ee2c5d7c20fcad
Jonah Duckles <jduckles@ou.edu>
2011-11-22 16:49:50 -0600

    deserialize pickled result strings and added comments

--- wsgi_apps/queue/queue_status.py
+++ wsgi_apps/queue/queue_status.py
@@ -51,24 +51,32 @@ class Root(object):
     @cherrypy.expose
     @mimetype('application/json')
     def run(self,*args,**kwargs):
+        # If no arguments return list of tasks
         if len(args) == 0:
             return json.dumps({'error': "Unknown task", 'available_tasks': list(REGISTERED_TASKS)}, indent=2)
         funcq = args[0].split('@') #split function queue 
-        funcname = funcq[0]
-        if len(funcq) == 1:
+        funcname = funcq[0] # Get function name for calling
+        if len(funcq) == 1: # Set default queue to celery if none defined
             queue = 'celery'
         else:
             queue = funcq[1]
-        if funcname not in REGISTERED_TASKS:
+        if funcname not in REGISTERED_TASKS: # Check if funcname is know, if not show possible names
             return json.dumps({'error': "Unknown task", 'available_tasks': list(REGISTERED_TASKS)}, indent=2)
-        if queue not in AVAILABLE_QUEUES:
+        if queue not in AVAILABLE_QUEUES: # Check if queue is known, if not show possible queue
             return json.dumps({'error': "Unknown queue", 'available_queues': list(AVAILABLE_QUEUES)}, indent=2)
-        funcargs = args[1:]
+        funcargs = args[1:] # Slice out function arguments for passing along to task.
+        
+        if kwargs.has_key('callback'):
+            callback = kwargs.pop('callback') # pop off callback so it doesn't get passed
+            kwargs.pop('_')
+        else:
+            callback = None
+        
         taskobj = send_task( funcname, args=funcargs, kwargs=kwargs, queue=queue, track_started=True )
-        if 'callback' not in kwargs:
+        if not callback:
             return json.dumps({'task_id':taskobj.task_id}, indent=2)
         else:
-            return str(kwargs['callback']) + "(" + json.dumps({'task_id':taskobj.task_id}, indent=2) + ")"
+            return str(callback) + "(" + json.dumps({'task_id':taskobj.task_id}, indent=2) + ")"
     @cherrypy.expose
     @mimetype('application/json')
     def task(self,task_id=None,type=None,callback=None,**kwargs):
@@ -82,8 +90,9 @@ class Root(object):
         if type == None:
             res = {}
             result = urllib.urlopen("http://fire.rccc.ou.edu/mongo/db_find/cybercom_queue/cybercom_queue_meta/{'spec':{'_id':'" + task_id + "'}}")
-            res["tombstone"]=json.loads(result.read())
-            res['tombstone'][0]['result'] = pickle.loads(res['tombstone'][0]['result'].encode())
+            res["tombstone"] = json.loads(result.read())
+            if len(res['tombstone']) > 0:
+                res['tombstone'][0]['result'] = pickle.loads(res['tombstone'][0]['result'].encode())
             res['status']=AsyncResult(task_id).status
             res['task_id']=task_id
             return json.dumps(res,indent=2)
