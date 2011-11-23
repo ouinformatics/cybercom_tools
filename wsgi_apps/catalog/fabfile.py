9187cceceb7e32fd2e679f8594c40fc4bb34bca4
Jonah Duckles <jduckles@ou.edu>
2011-11-23 14:38:32 -0600

    Linking to common fabfile

--- wsgi_apps/catalog/fabfile.py
+++ wsgi_apps/catalog/fabfile.py
@@ -1,29 +1,33 @@
 from fabric.api import *
-from fabric.contrib.files import exists
+from fabric.contrib.files import exists, append, comment
 from fabric.colors import red
+import os 
 
-env.sitename = 'catalog'
+env.sitename = os.path.basename(os.getcwd())
 env.mongo_host = 'fire.rccc.ou.edu'
 env.psql_host = 'fire.rccc.ou.edu'
-env.path = '/scratch/www/wsgi_sites/%(sitename)s' % env
-env.virtpy = '/scratch/www/wsgi_sites/%(sitename)s/virtpy' % env
-env.log_path = '/scratch/www/wsgi_sites/%(sitename)s/log' % env
-env.templates = '/scratch/www/html/templates/%(sitename)s' % env
-env.apache_config_path = '/etc/httpd/conf.d/%(sitename)s' % env
-env.python = 'python2.6'
+env.apache_config = '/etc/httpd/conf.d/%(sitename)s.conf' % env
+env.python = '/usr/bin/python2.6'
+
  
-def staging():
+def testing():
     """
     Work on staging environment
     """
     env.settings = 'testing'
-    env.hosts = ['fire.rccc.ou.edu']
+    env.path = '/var/www/apps/%(sitename)s' % env
+    env.virtpy = '%(path)s/virtpy' % env
+    env.log_path = '%(path)s/log' % env
+    env.hosts = ['test.cybercommons.org']
 
 def production():
     """
     Work on production environment
     """
     env.settings = 'production'
+    env.path = '/scratch/www/wsgi_sites/%(sitename)s' % env
+    env.virtpy = '%(path)s/virtpy' % env
+    env.log_path = '%(path)s/log' % env
     env.hosts = ['fire.rccc.ou.edu']
     
 def setup():
@@ -33,55 +37,72 @@ def setup():
     """
     setup_directories()
     copy_working_dir()
-    #place_templates()
     setup_virtualenv()
     install_requirements()
+    apache_config()
     bounce_apache()
 
 def deploy():
-    """ Deploy code changes """
+    """
+    Deploy changes which don't impact virtual environment
+    """ 
     copy_working_dir()
-    #place_templates()
     bounce_apache()
-
-
-def update_cybercom():
-    run('source %(path)s/virtpy/bin/activate; pip install -U https://github.com/ouinformatics/cybercom/tarball/master' % env)
-    bounce_apache()
-
 
 def setup_directories():
-    """ Make directories """
-    run('mkdir -p %(path)s' % env)
-    run('mkdir -p %(log_path)s' % env)
-    run('mkdir -p %(virtpy)s' % env)
-    sudo('mkdir -p %(templates)s' % env)
+    """ 
+    Setup directories on the remote system
+    """
+    if not exists('%(path)s' % env):
+        run('mkdir -p %(path)s' % env)
+        run('mkdir -p %(log_path)s' % env)
+        run('mkdir -p %(virtpy)s' % env)
+
+
+def virtualenv(command):
+    """ 
+    Wrapper to activate and run virtual environment
+    """
+    with cd(env.virtpy):
+        run('source %(virtpy)s/bin/activate' % env + '&&' + command)
+
+def setup_virtualenv():
+    """
+    Install the virtual environment
+    """
+    run('virtualenv -p %(python)s --no-site-packages %(virtpy)s' % env)
 
 def bounce_apache():
     """ Restart the apache web server """
     sudo('/etc/init.d/httpd restart')
 
-def place_templates():
-    checktemp = exists('%(path)s/templates/' % env)
-    if checktemp:
-        sudo('mkdir -p %(templates)s' % env)
-        sudo('cp %(path)s/templates/* %(templates)s' %env)
-
-def setup_virtualenv():
-    """ Create a virtual environment """
-    run('virtualenv -p %(python)s --no-site-packages %(virtpy)s' % env)
+def apache_config():
+    """
+    Set the apache config file to point to wsgi.  Assumes app will be accessible at /sitename/ and 
+      .wsgi named sitename.wsgi
+    """
+    # check if apache config lines exist in old wsgi_sites.conf and comment if found
+    comment('/etc/httpd/conf.d/wsgi_sites.conf', r'^WSGIScriptAlias /%(sitename)s .*$' % env, use_sudo=True)
+    confline = 'WSGIScriptAlias /%(sitename)s %(path)s/%(sitename)s.wsgi' %env
+    append('%(apache_config)s' % env, confline, use_sudo=True)
 
 def copy_working_dir():
-    """ tgz the working directory and send it to remote host """
+    """
+    Shuttle application code from local to remote
+    """
     local('tar --exclude virtpy -czf /tmp/deploy_%(sitename)s.tgz .' % env)
     put('/tmp/deploy_%(sitename)s.tgz' % env, '%(path)s/deploy_%(sitename)s.tgz' % env)
     run('cd %(path)s; tar -xf deploy_%(sitename)s.tgz; rm deploy_%(sitename)s.tgz' % env)
     local('rm /tmp/deploy_%(sitename)s.tgz' % env)
 
 def install_requirements():
-    """ Install virtualpython requirements """
-    checkreq = exists('%(path)s/requirements.txt' % env)
-    if checkreq:
-        run('source %(virtpy)s/bin/activate; pip install numpy; pip install -E %(virtpy)s -r %(path)s/requirements.txt' % env)
+    """ 
+    Install the contents of requirements.txt to the virtual environment 
+    """
+    check = exists('%(path)s/requirements.txt' % env)
+    if check:
+        virtualenv('pip install -E %(virtpy)s -r %(path)s/requirements.txt' % env)
     else:
         print red("Can't find requirements.txt!")
+
+
